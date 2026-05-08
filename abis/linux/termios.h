a72d6489669941384ef3f5e7a6feee7d06a5c375
Alexander Richards <electrodeyt@gmail.com>
2026-05-09 00:15:19 +0200

    Move termios constants into ABI headers

--- abis/linux/termios.h
+++ abis/linux/termios.h
@@ -5,6 +5,66 @@ typedef unsigned char cc_t;
 typedef unsigned int speed_t;
 typedef unsigned int tcflag_t;
 
+/* baud rate constants for speed_t */
+#define B0       0
+#define B50      1
+#define B75      2
+#define B110     3
+#define B134     4
+#define B150     5
+#define B200     6
+#define B300     7
+#define B600     8
+#define B1200    9
+#define B1800    10
+#define B2400    11
+#define B4800    12
+#define B9600    13
+#define B19200   14
+#define B38400   15
+#define B57600   0010001
+#define B115200  0010002
+#define B230400  0010003
+#define B460800  0010004
+#define B500000  0010005
+#define B576000  0010006
+#define B921600  0010007
+#define B1000000 0010010
+#define B1152000 0010011
+#define B1500000 0010012
+#define B2000000 0010013
+#define B2500000 0010014
+#define B3000000 0010015
+#define B3500000 0010016
+#define B4000000 0010017
+
+/* constants for tcsetattr() */
+#define TCSANOW 0
+#define TCSADRAIN 1
+#define TCSAFLUSH 2
+
+/* constants for tcflush() */
+#define TCIFLUSH 0
+#define TCOFLUSH 1
+#define TCIOFLUSH 2
+
+/* constants for tcflow() */
+#define TCOOFF 0
+#define TCOON 1
+#define TCIOFF 2
+#define TCION 3
+
+#if defined(_DEFAULT_SOURCE)
+#define TIOCM_DTR 0x002
+#define TIOCM_RTS 0x004
+#define TIOCM_CTS 0x020
+#define TIOCM_CAR 0x040
+#define TIOCM_RNG 0x080
+#define TIOCM_DSR 0x100
+#define TIOCM_RI TIOCM_RNG
+#define TIOCM_CD TIOCM_CAR
+#endif /* defined(_DEFAULT_SOURCE) */
+
 /* indices for the c_cc array in struct termios */
 #define NCCS     32
 #define VINTR    0
@@ -141,5 +201,14 @@ struct termios {
 	speed_t c_ibaud;
 	speed_t c_obaud;
 };
+
+#if defined(_DEFAULT_SOURCE)
+#define TIOCSCTTY 0x540E
+#define TIOCGPGRP 0x540F
+#define TIOCSPGRP 0x5410
+#define TIOCGWINSZ 0x5413
+#define TIOCSWINSZ 0x5414
+#define TIOCGSID 0x5429
+#endif /* defined(_DEFAULT_SOURCE) */
 
 #endif
